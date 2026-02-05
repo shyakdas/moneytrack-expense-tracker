95fdc0837c4b288ba050fab921b19fe41b50165c
Shyak Das <shayak.das007@gmail.com>
2026-02-05 19:29:58 +0530

    Fixing the namespace

--- app/src/main/java/MainActivity.kt
+++ app/src/main/java/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.MoneyTrack
+package com.moneytrack
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -11,7 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.MoneyTrack.ui.theme.MoneyTrackTheme
+import com.moneytrack.ui.theme.MoneyTrackTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
