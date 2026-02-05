15390346cb315fcbdc4e6a98287869981f841929
Shyak Das <shayak.das007@gmail.com>
2026-02-06 00:04:39 +0530

    fix

--- app/src/main/java/com/moneytrack/MainActivity.kt
+++ app/src/main/java/com/moneytrack/MainActivity.kt
@@ -17,17 +17,3 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-
-// NO Need
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-}
