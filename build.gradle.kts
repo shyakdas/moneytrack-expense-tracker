860a725effbd9ff51586b6d892e288e3c3977d30
Shyak Das <shayak.das007@gmail.com>
2026-02-05 18:21:01 +0530

    Setting up project

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,8 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.compose) apply false
+    // Zero style debates, Consistent diffs, Fast PR reviews
+    id("org.jlleitschuh.gradle.ktlint") version "14.0.1" apply false
+    // God classes, Long methods, Bad Kotlin patterns
+    id("io.gitlab.arturbosch.detekt") version "1.23.8" apply false
 }
