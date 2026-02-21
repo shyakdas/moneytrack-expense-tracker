f7d5af6285ba2e5da611a34631e5b57dcc0b1691
Shyak Das <shayak.das007@gmail.com>
2026-02-21 16:02:42 +0530

    feat(theme): add dark mode support using semantic color mapping

--- core/src/main/java/ui/theme/Color.kt
+++ core/src/main/java/ui/theme/Color.kt
@@ -5,71 +5,68 @@ package ui.theme
 import androidx.compose.ui.graphics.Color
 
 // --------------------
-// Dark Scale
+// Violet Scale
 // --------------------
-val Dark25 = Color(0xFFF5F5F5)
-val Dark50 = Color(0xFFBDBDBD)
-val Dark75 = Color(0xFF616161)
-val Dark100 = Color(0xFF121212)
-
+val Violet100 = Color(0xFF7F3DFF)
+val Violet80  = Color(0xFF8F57FF)
+val Violet60  = Color(0xFFB18AFF)
+val Violet40  = Color(0xFFD3BDFF)
+val Violet20  = Color(0xFFEEE5FF)
 
 // --------------------
 // Light Scale
 // --------------------
-val Light20 = Color(0xFFFFFFFF)
-val Light40 = Color(0xFFF8F9FA)
-val Light60 = Color(0xFFE9ECEF)
-val Light80 = Color(0xFFE2E6EA)
-val Light100 = Color(0xFFDEE2E6)
-
-
-// --------------------
-// Violet Scale
-// --------------------
-val Violet20 = Color(0xFFF3E8FF)
-val Violet40 = Color(0xFFD8B4FE)
-val Violet60 = Color(0xFFA855F7)
-val Violet80 = Color(0xFF7E22CE)
-val Violet100 = Color(0xFF4C1D95)
-
+val Light100 = Color(0xFFFFFFFF)
+val Light80  = Color(0xFFFCFCFC)
+val Light60  = Color(0xFFF1F1FA)
+val Light40  = Color(0xFFE3E5E5)
+val Light20  = Color(0xFF91919F)
 
 // --------------------
-// Red Scale
+// Dark Scale
 // --------------------
-val Red20 = Color(0xFFFEE2E2)
-val Red40 = Color(0xFFFCA5A5)
-val Red60 = Color(0xFFEF4444)
-val Red80 = Color(0xFFB91C1C)
-val Red100 = Color(0xFF7F1D1D)
-
-
-// --------------------
-// Green Scale
-// --------------------
-val Green20 = Color(0xFFDCFCE7)
-val Green40 = Color(0xFF86EFAC)
-val Green60 = Color(0xFF22C55E)
-val Green80 = Color(0xFF15803D)
-val Green100 = Color(0xFF14532D)
-
-
-// --------------------
-// Yellow Scale
-// --------------------
-val Yellow20 = Color(0xFFFEF9C3)
-val Yellow40 = Color(0xFFFDE047)
-val Yellow60 = Color(0xFFEAB308)
-val Yellow80 = Color(0xFFCA8A04)
-val Yellow100 = Color(0xFF854D0E)
-
+val Dark100 = Color(0xFF0D0E0F)
+val Dark75  = Color(0xFF161719)
+val Dark50  = Color(0xFF212325)
+val Dark25  = Color(0xFF292B2D)
 
 // --------------------
 // Blue Scale
 // --------------------
-val Blue20 = Color(0xFFDBEAFE)
-val Blue40 = Color(0xFF93C5FD)
-val Blue60 = Color(0xFF3B82F6)
-val Blue80 = Color(0xFF1D4ED8)
-val Blue100 = Color(0xFF1E3A8A)
+val Blue100 = Color(0xFF0077FF)
+val Blue80  = Color(0xFF248AFF)
+val Blue60  = Color(0xFF57A5FF)
+val Blue40  = Color(0xFF8AC0FF)
+val Blue20  = Color(0xFFBDDCFF)
 
+// --------------------
+// Red Scale
+// --------------------
+val Red100 = Color(0xFFFD3C4A)
+val Red80  = Color(0xFFFD5662)
+val Red60  = Color(0xFFFD6F7A)
+val Red40  = Color(0xFFFDA2A9)
+val Red20  = Color(0xFFFDD5D7)
+
+// --------------------
+// Green Scale
+// --------------------
+val Green100 = Color(0xFF00A86B)
+val Green80  = Color(0xFF2AB784)
+val Green60  = Color(0xFF65D1AA)
+val Green40  = Color(0xFF93EACA)
+val Green20  = Color(0xFFCFFAEA)
+
+// --------------------
+// Yellow Scale
+// --------------------
+val Yellow100 = Color(0xFFFCAC12)
+val Yellow80  = Color(0xFFFCBB3C)
+val Yellow60  = Color(0xFFFCCC6F)
+val Yellow40  = Color(0xFFFCDDA1)
+val Yellow20  = Color(0xFFFCEED4)
+
+// --------------------
+// Neutral
+// --------------------
 val NeutralC6 = Color(0xFFC6C6C6)
