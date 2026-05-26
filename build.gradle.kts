187aa463738ec7b73bac5e5e976d53b5420f1aa4
Shyak Das <shayak.das007@gmail.com>
2026-05-26 23:33:55 +0530

    fix(security): harden workflows and patch vulnerable transitive deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,6 +25,28 @@ plugins {
     id("com.google.gms.google-services") version "4.4.4" apply false
 }
 
+buildscript {
+    dependencies {
+        constraints {
+            // Security overrides for transitive build-time dependencies
+            classpath("org.bouncycastle:bcpkix-jdk18on:1.80")
+            classpath("org.bouncycastle:bcprov-jdk18on:1.80")
+            classpath("org.bouncycastle:bcutil-jdk18on:1.80")
+            classpath("org.bitbucket.b_c:jose4j:0.9.6")
+            classpath("org.jdom:jdom2:2.0.6.1")
+            classpath("org.apache.commons:commons-compress:1.28.0")
+            classpath("org.apache.commons:commons-lang3:3.17.0")
+            classpath("ch.qos.logback:logback-core:1.5.18")
+
+            // Netty family advisories
+            classpath("io.netty:netty-codec:4.1.118.Final")
+            classpath("io.netty:netty-codec-http:4.1.118.Final")
+            classpath("io.netty:netty-codec-http2:4.1.118.Final")
+            classpath("io.netty:netty-handler:4.1.118.Final")
+        }
+    }
+}
+
 subprojects {
 
     plugins.withId("com.android.application") {
